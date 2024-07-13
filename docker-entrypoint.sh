42d7e8a489882a6997d810ddebfa2e87a283fe22
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2024-07-13 16:53:42 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,30 +1,20 @@
 #!/bin/sh
 
-# Correct permissions for Squid log directory
+# Correct permissions for Squid log and cache directories
 chown -R squid:squid /var/log/squid /var/spool/squid
 
 # Initialize Squid cache directories if they don't exist
-if [ ! -d "/var/spool/squid/01" ]; then
+if [ ! -d "/var/spool/squid/00" ]; then
     echo "Initializing Squid cache directories..."
-    squid -N -z
+    squid -z
+    sleep 5 # Wait a few seconds to ensure directories are created
 fi
 
-# Adjustments for cache memory and maximum object size
-if [ ! -z "$SQUID_CACHE_SIZE" ]; then
-    sed -i "s/^cache_dir ufs \/var\/spool\/squid .*/cache_dir ufs \/var\/spool\/squid ${SQUID_CACHE_SIZE:-1000} 16 256/" /etc/squid/squid.conf
-fi
-
-if [ ! -z "$SQUID_CACHE_MEM" ]; then
-    sed -i "s/^cache_mem .*/cache_mem ${SQUID_CACHE_MEM:-256} MB/" /etc/squid/squid.conf
-fi
-
-if [ ! -z "$SQUID_MAX_OBJ_SIZE" ]; then
-    sed -i "s/^maximum_object_size .*/maximum_object_size ${SQUID_MAX_OBJ_SIZE:-128} MB/" /etc/squid/squid.conf
-fi
-
-if [ ! -z "$SQUID_WORKERS" ]; then
-    sed -i "s/^workers .*/workers ${SQUID_WORKERS:-2}/" /etc/squid/squid.conf
-fi
+# Adjustments for cache memory, maximum object size, and workers
+[ ! -z "$SQUID_CACHE_SIZE" ] && sed -i "s/^cache_dir ufs \/var\/spool\/squid .*/cache_dir ufs \/var\/spool\/squid ${SQUID_CACHE_SIZE:-100} 16 256/" /etc/squid/squid.conf
+[ ! -z "$SQUID_CACHE_MEM" ] && sed -i "s/^cache_mem .*/cache_mem ${SQUID_CACHE_MEM:-256} MB/" /etc/squid/squid.conf
+[ ! -z "$SQUID_MAX_OBJ_SIZE" ] && sed -i "s/^maximum_object_size .*/maximum_object_size ${SQUID_MAX_OBJ_SIZE:-128} MB/" /etc/squid/squid.conf
+[ ! -z "$SQUID_WORKERS" ] && sed -i "s/^workers .*/workers ${SQUID_WORKERS:-2}/" /etc/squid/squid.conf
 
 # Start Squid
 exec squid -f /etc/squid/squid.conf -NYCd 1
